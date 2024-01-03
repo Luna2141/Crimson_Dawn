c53cd979807ed9edf2d23fce795b12335d637f37
Luna2141 <60728436+Luna2141@users.noreply.github.com>
2024-01-03 17:17:59 -0500

    added yew support

--- Website/Crimson-landing/src/main.rs
+++ Website/Crimson-landing/src/main.rs
@@ -1,4 +1,7 @@
-#[macro_use] extern crate rocket;
+use yew::prelude::*;
+
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -8,4 +11,27 @@ fn index() -> &'static str {
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
+}
+
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <button onclick=onclick>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
+}
+
+fn main() {
+    yew::start_app::<App>();
 }
