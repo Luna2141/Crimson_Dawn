10b956fa8f83a361a158f7987be12a83485a402e
Luna2141 <60728436+Luna2141@users.noreply.github.com>
2024-01-05 16:08:01 -0500

    added yew-router support

--- Website/Main-Site/src/main.rs
+++ Website/Main-Site/src/main.rs
@@ -1,23 +1,15 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
 
 #[function_component]
 fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
+
     };
 
-    html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-        </div>
-    }
+
 }
+
+
 
 fn main() {
     yew::Renderer::<App>::new().render();
